cf554ae31c408d4692c30a8a93b104916632e7cc
Al Azad <144646711+alazad214@users.noreply.github.com>
2024-09-29 08:43:03 +0600

    Add files via upload

---
+++ language/ban.dart
@@ -0,0 +1,5 @@
+const Map<String, String> ban = {
+  'hello': 'হ্যালো',
+  'welcome': 'স্বাগতম',
+  'select_language': 'ভাষা নির্বাচন করুন',
+};
---
+++ language/eng.dart
@@ -0,0 +1,5 @@
+const Map<String, String> eng = {
+  'hello': 'Hello',
+  'welcome': 'Welcome',
+  'select_language': 'Select Language',
+};
